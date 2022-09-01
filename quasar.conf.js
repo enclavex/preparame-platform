6c883196ab9fad76df5a61aeeb2a387b33c89939
EnclaveX <contatoenclave@gmail.com>
2022-09-01 15:34:09 -0300

    teste ajuste history mode

--- quasar.conf.js
+++ quasar.conf.js
@@ -48,8 +48,7 @@ module.exports = function (/* ctx */) {
 
     // Full list of options: https://v1.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
-      vueRouterMode: 'history', // available values: 'hash', 'history'
-      publicPath: './',
+      vueRouterMode: 'hash', // available values: 'hash', 'history'
 
       // transpile: false,
 
