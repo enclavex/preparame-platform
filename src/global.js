5c5924fda269630adc149c69c02e7813daafbf55
EnclaveX <contatoenclave@gmail.com>
2023-04-29 10:27:43 -0300

    correão final

--- src/global.js
+++ src/global.js
@@ -4,11 +4,11 @@ export const userKey = "__knowledge_user"
 
 var baseApiUrlDefinition = ""
 
-if (process.env.NODE_ENV && process.env.NODE_ENV == 'development') {
-    baseApiUrlDefinition = "http://localhost:3334"
-} else {
-    baseApiUrlDefinition = "http://3.91.64.166"
-}
+// if (process.env.NODE_ENV && process.env.NODE_ENV == 'development') {
+//     baseApiUrlDefinition = "http://localhost:3334"
+// } else {
+    baseApiUrlDefinition = "https://3.91.64.166"
+// }
 
 export const baseApiUrl = baseApiUrlDefinition
 
