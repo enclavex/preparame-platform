247de949b40b00ce242dd306a97f57529d4dd6a2
EnclaveX <contatoenclave@gmail.com>
2023-05-12 21:51:55 -0300

    caminho api

--- src/global.js
+++ src/global.js
@@ -7,7 +7,7 @@ var baseApiUrlDefinition = ""
 if (process.env.NODE_ENV && process.env.NODE_ENV == 'development') {
     baseApiUrlDefinition = "http://localhost:3334"
 } else {
-    baseApiUrlDefinition = "https://api.prepara.me:3334"
+    baseApiUrlDefinition = "http://api.prepara.me:3334"
 }
 
 export const baseApiUrl = baseApiUrlDefinition
